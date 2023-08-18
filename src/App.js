2bcab01d652c99c9a04016b35354b8a286602605
Jangwoo Park <p.jangwoo@gmail.com>
2023-08-18 14:38:28 +0900

    8월 18일

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import Lab1 from './components/Lab1'; 
+import Lab2 from './components/Lab2'; 
+import Lab3 from './components/Lab3'; 
+import Lab4 from './components/Lab4';
+import Lab5 from './components/Lab5';
+import Lab6 from './components/Lab6'; 
+import Lab7 from './components/Lab7';
+import Lab8 from './components/Lab8';
+
+
+function App() {
+  return (
+    <div>
+        <h1> useState 의 실습 </h1>
+        <hr />
+        <Lab1 />
+        <hr />
+        <Lab2 />
+        <hr />
+        <Lab3 />
+        <hr />
+        <Lab4 />
+        <hr />
+        <Lab5 />
+        <hr />
+        <Lab6 />
+        <hr />
+        <Lab7 /> <br /> <br /> <br />
+        <hr />
+        <Lab8 />
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
